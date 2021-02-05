31b4d4128ed6049f5b75ab93470f6c71fc14e08b
Evelyn19-Ulco <76453340+Evelyn19-Ulco@users.noreply.github.com>
2021-02-05 11:43:57 -0500

    Add files via upload

---
+++ forange.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 25 15:13:51 2021
+
+@author: DELL
+"""
+
+print ("Comienzo")
+for i in range(10,0,-1):
+    print(i, end= " ")
+print()
+print("Final")
